3ae1d935d41e0e40695c0b4afc3f2d777095b5f8
shandianchengzi <shenshandian@hust.edu.cn>
2024-09-06 17:27:02 +0800

    feat(pubmed): 加上了从pubmed数据源获取数据的功能，不过尚未测试。

--- zotero-dblp.js
+++ zotero-dblp.js
@@ -7,7 +7,28 @@ function fetchWithTimeout(url, timeout = 3000) {
   ]);
 }
 
-async function getBibtexContent(bibtexLink) {
+async function getBibtexContent(bibtexLink, method = "get") {
+  // for post
+  if (method == "post") {
+    const response = await fetchWithTimeout(bibtexLink, {
+      method: "POST",
+      body: "format=pubmed",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then(async (bibtexContent) => {
+        // 处理响应
+        bibtexContent = await bibtexContent.text();
+        return bibtexContent;
+      })
+      .catch((error) => {
+        // 处理错误
+        return "BibTeX 链接请求超时！";
+      });
+    return response;
+  }
+  // for get (default)
   const response = await fetchWithTimeout(bibtexLink)
     .then(async (bibtexContent) => {
       // 处理响应
@@ -21,7 +42,7 @@ async function getBibtexContent(bibtexLink) {
   return response;
 }
 
-async function findDomFromUrl(url, dom) {
+async function findDomFromUrl(url, dom, dom_feature) {
   const response = await fetchWithTimeout(url)
     .then(async (searchHtml) => {
       // 处理响应
@@ -34,7 +55,7 @@ async function findDomFromUrl(url, dom) {
       // 查找第一个条目的 dom 链接
       const firstEntryBibtexLink = doc.querySelector(dom);
 
-      const domLink = firstEntryBibtexLink.href; // link or undefined
+      const domLink = firstEntryBibtexLink.getAttribute(dom_feature); // link or undefined
       if (domLink) {
         return domLink;
       } else {
@@ -57,7 +78,7 @@ function createNewItem(item, newItemTypeID) {
   return newItem;
 }
 
-function updateItem(item, newItem, newPrior=false) {
+function updateItem(item, newItem, newPrior = false) {
   // if newPrior is true, then newItem's value will be prior
   let itemTypeID = item.getType();
 
@@ -94,15 +115,16 @@ function updateItem(item, newItem, newPrior=false) {
   // 当且仅当旧条目中没有作者或编辑时，才将新条目的作者或编辑添加到旧条目中；如果newPrior为true，则反之
   for (let c of oldCreators) {
     let creatorType = Zotero.CreatorTypes.getName(c.creatorTypeID);
-    if (creatorType == "author")
-      needAddAuthor = false;
-    else if (creatorType == "editor")
-      needAddEditor = false;
-      finalCreators.push(c);
+    if (creatorType == "author") needAddAuthor = false;
+    else if (creatorType == "editor") needAddEditor = false;
+    finalCreators.push(c);
   }
   for (let c of newCreators) {
     let creatorType = Zotero.CreatorTypes.getName(c.creatorTypeID);
-    if ((creatorType == "author" && needAddAuthor) || creatorType == "editor" && needAddEditor)
+    if (
+      (creatorType == "author" && needAddAuthor) ||
+      (creatorType == "editor" && needAddEditor)
+    )
       finalCreators.push(c);
   }
   item.setCreators(finalCreators);
@@ -157,7 +179,6 @@ const searchUrlBases = {
       {
         url: "https://scholar.google.com/scholar?q=@@/&output=cite",
         dom: "a.gs_citi", // 'div.gs_r',
-        keyword_regex: {},
       },
       // {
       //   url: "info:@@:scholar.google.com/",
@@ -165,7 +186,37 @@ const searchUrlBases = {
       // }
     ],
   },
+  pubmed: {
+    bibtex_route: [
+      {
+        url: "https://pubmed.ncbi.nlm.nih.gov/?term=@@",
+        dom: "div.cite",
+        dom_feature: "data-pubmed-format-link",
+        method: "post",
+        format: "nbib",
+      },
+    ],
+  },
 };
+
+// init default searchUrlBases
+for (let searchUrlBaseName in searchUrlBases) {
+  let searchUrlBase = searchUrlBases[searchUrlBaseName];
+  for (let bibtex_route of searchUrlBase.bibtex_route) {
+    if (!bibtex_route.keyword_regex) {
+      bibtex_route.keyword_regex = {};
+    }
+    if (!bibtex_route.method) {
+      bibtex_route.method = "get";
+    }
+    if (!bibtex_route.dom_feature) {
+      bibtex_route.dom_feature = "href";
+    }
+    if (!bibtex_route.format) {
+      bibtex_route.format = "bibtex";
+    }
+  }
+}
 
 var zoteroPane = Zotero.getActiveZoteroPane();
 var items = zoteroPane.getSelectedItems();
@@ -196,7 +247,12 @@ for (let item of items) {
           "@@",
           encodeURIComponent(keyword)
         );
-        keyword = await findDomFromUrl(searchUrl, bibtex_route.dom);
+
+        keyword = await findDomFromUrl(
+          searchUrl,
+          bibtex_route.dom,
+          bibtex_route.dom_feature
+        );
         if (!keyword) {
           addUnavailableItem(
             item,
@@ -206,6 +262,7 @@ for (let item of items) {
           findError = true;
           break;
         }
+
         // 处理关键词
         let keyword_regex = bibtex_route.keyword_regex;
         for (let regex in keyword_regex) {
@@ -220,7 +277,10 @@ for (let item of items) {
 
       // 访问 BibTex 链接并获取 BibTex 内容
       let bibtexLink = keyword;
-      const bibtexContent = await getBibtexContent(bibtexLink);
+      const bibtexContent = await getBibtexContent(
+        bibtexLink,
+        searchUrlBase.bibtex_route.method
+      );
 
       // 通过 Translator 'BibTex' 解析 BibTeX 内容
       var translate = new Zotero.Translate.Import();
@@ -234,7 +294,7 @@ for (let item of items) {
         item = updateItem(item, parsedBibtexItem, false);
         // 保存更新后的条目
         await item.saveTx();
-      }else{
+      } else {
         // 创建新条目
         let newItem = createNewItem(item, parsedBibtexItem.getType());
         // 更新新条目信息
